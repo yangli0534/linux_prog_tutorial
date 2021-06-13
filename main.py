7ab65e04280cde7b5c37ef4cc78cc83da5640f6f
leonli <yangli0534@yahoo.com>
2021-06-14 00:06:55 +0800

    read temp

--- main.py
+++ main.py
@@ -18,12 +18,15 @@ def print_time():
     dt = dt.strftime("%H:%M:%S %d/%m/%y")
     print(f'time = {dt}')  # Press ⌘F8 to toggle the breakpoint.
     temp = psutil.sensors_temperatures()['coretemp']
+    #temp = max(temp[0].current, temp[1].current, temp[2].current, temp[3].current)
 
     with open(path, 'a+') as file:
 
-        file.write(f'time = {dt}: {temp[0].current}, {temp[1].current}, {temp[2].current}, {temp[3].current}\n')
+        file.write(f'time = {dt}: server temp = {temp}')
+        for i, sensor in enumerate(temp):
+            file.write(f'sensor {sensor.label}: {sensor.current}')
 
-
+        file.write('\n')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
