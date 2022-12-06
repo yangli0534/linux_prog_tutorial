70c488d5696e45d0b9f1be0e877f7c2394bd721b
yangli0534 <yangli0534@yahoo.com>
2022-12-06 20:31:12 +0800

    Update BookLibrary.py

--- BookLibrary.py
+++ BookLibrary.py
@@ -12,7 +12,7 @@ import psutil
 from getpass import getpass
 from mysql.connector import connect, Error
 
-import Book
+from Book import Book
 
 class BookLibrary():
     def __init__(self, ip, user, password, port=3306, database='book'):
@@ -30,7 +30,7 @@ class BookLibrary():
 
     def query_with_id(self, id):
         query = f""" SELECT * FROM book.books WHERE zlibrary_id = {id};"""
-
+        query_isbn = f""" SELECT * FROM book.isbn WHERE zlibrary_id = {id};"""
         try:
             with self.connection.cursor() as cursor:
                 for result in cursor.execute(query, multi=True):
@@ -40,14 +40,42 @@ class BookLibrary():
 
                 self.connection.commit()
                 # TODO: bug TypeError: 'module' object is not callable
-                book = Book(info=tmp[0])
-                return book
+
                 # time.sleep(1)
         except Error as e:
             # print('sql operation failed!')
             print(e)
 
+        try:
+            with self.connection.cursor() as cursor:
+                for result in cursor.execute(query_isbn, multi=True):
+                    if result.with_rows:
+                        tmp1 = result.fetchall()
+                        # print(tmp)
+
+                self.connection.commit()
+                # TODO: bug TypeError: 'module' object is not callable
+
+                # time.sleep(1)
+        except Error as e:
+            # print('sql operation failed!')
+            print(e)
+        if len(tmp) > 0 and len(tmp1) > 0:
+            book = Book(info=tmp[0], isbn=tmp1[0])
+            return book
+        elif len(tmp) == 0:
+            book = Book(info=None, isbn=None)
+            return book
+        elif len(tmp1) == 0:
+            book = Book(info=tmp[0], isbn=None)
+            return book
+
 if __name__ == '__main__':
-    lib = BookLibrary(ip='10.10.10.119', user='leon', password='oppaha89@A')
-    book = lib.query_with_id(id=589518)
-    print(book.title)
+    lib = BookLibrary(ip='172.16.1.35', user='root', password='leonfromzillnk')
+    #book = lib.query_with_id(id=18419)
+    for i in range(177, 200):
+        book = lib.query_with_id(id=i)
+        # print(book.title)
+        # print(book.author)
+        # print(book.isbn)
+        print(f'zlibrary id = {i}, book name = {book.title}, book author = {book.author}, isbn = {book.isbn}')
