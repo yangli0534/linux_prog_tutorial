f97299666e76a061b485f7cf9fde83621fd70eca
yangli0534 <yangli0534@yahoo.com>
2022-12-08 00:47:52 +0800

    add class Book

--- book_query.py
+++ book_query.py
@@ -16,11 +16,13 @@ import os,sys
 #import psutil
 from BookLibrary import BookLibrary
 from slugify import slugify
+from pathvalidate import sanitize_filename
 
 lib = BookLibrary(host='10.10.10.250', user='root', database='library', password='oppaha89@A')
 
-directory = '/volume2/library/books/pilimi-zlib-120000-419999/'
-dest = '/volume2/library/book_lib'
+# directory = '/volume2/library/books/tmp'
+directory = '/volume2/library/books/pilimi-zlib-0-119999'
+dest = '/volume2/library/book_lib/'
 N = 20
 i = 0
 for filename in os.listdir(directory):
@@ -28,12 +30,21 @@ for filename in os.listdir(directory):
 
     # checking if it is a file
     i = i + 1
-    if os.path.isfile(f) and i < N:
+    if os.path.isfile(f):
 
         book = lib.query_with_id(id=int(filename))
-        print(f'filename = {filename}, book title = {book.title} and author = {book.author}')
-        new_name = slugify(f'{book.title}_{book.author}', max_length=80, word_boundary=False, separator=' ',)
-
+        print(f'i={i}: filename = {filename}, book title = {book.title} and author = {book.author},extension={book.extension}')
+        new_name = sanitize_filename(f'{book.title}_{book.author}')
+        if len(new_name) > 80:
+            new_name = new_name[0:79]
+        if book.language is None:
+            language = 'none'
+        else:
+            language = book.language
         new_name = f'{new_name}-{filename}.{book.extension}'
-        f_dst = os.path.join(dest, new_name)
+        sub_folder = str(int(i/100000))
+        dest_new = f'{dest}{language}/{sub_folder}'
+
+        f_dst = os.path.join(dest_new, new_name)
+        os.makedirs(dest_new, exist_ok=True)
         os.link(f, f_dst)
